414c9a1a6717f00ac4e3fb7584796fe7b1575983
KevinAlpizarDev <kevinalpizardev@gmail.com>
2024-08-12 10:47:55 -0600

    Befro problems

---
+++ secction_4/48.String_Interpolation.rb
@@ -0,0 +1,11 @@
+name = "Kevin"
+puts "Hello my name is #{name}!"
+puts "Hello #{name}, how are you?"
+
+age = 26
+puts "I am #{age} years old"
+puts "In 5 year, i will be #{age + 5} years old "
+
+x = 5 
+y = 8 
+puts "The sun of #{y} if #{x + y}"
---
+++ secction_4/51.Methods_to_Convert_Objects.rb
@@ -0,0 +1,32 @@
+text = "5"
+puts text#5
+puts text.class#String
+
+#Convert
+puts text.to_i.class#Integer
+puts "15 apples".to_i#15
+puts "apples 15".to_i#0
+puts "nonsense".to_i#0
+
+puts text.to_f#5.0
+puts text.to_f.class#Float
+
+number = 5 
+
+puts number.class#Integer
+puts number.to_s#"5"
+puts number.to_s.class#String
+
+puts 
+
+percentage = 0.9
+puts percentage.class
+puts percentage.to_s.class
+# puts percentage.class
+
+
+puts 
+puts "5".to_s
+puts "5".to_s.class
+
+
